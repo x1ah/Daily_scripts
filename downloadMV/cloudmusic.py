de066ea8675ed46159521ee870a4ce52096cd9c0
Xia h <Xia h>
2016-04-15 16:32:03 +0800

    update

--- downloadMV/cloudmusic.py
+++ downloadMV/cloudmusic.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 
 import urllib
 import re
 import os
 
+
 def gethtml(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
+
 
 def getvedio(html):
     mvpatt = r'hurl=(.+?\.jpg)'
@@ -23,12 +25,15 @@ def callbackfunc(already, a_ll, remote):
         persent = 100
     print '%.2f%%\r' % persent, '已经下载： ', a*b, '文件大小: ', c
 
+
 def main(num):
     html = gethtml('http://music.163.com/mv?id=%s' % num)
     vedio_list = getvedio(html)
     vedioUrls = vedio_list[0].split("&")
-    songer = vedioUrls[4].split('=')[1].decode('utf-8').strip().replace(' ', '-')
-    song_name = vedioUrls[3].split('=')[1].decode('utf-8').strip().replace(' ', '-')
+    songer = vedioUrls[4].split('=')[1].decode(
+        'utf-8').strip().replace(' ', '-')
+    song_name = vedioUrls[3].split('=')[1].decode(
+        'utf-8').strip().replace(' ', '-')
     store_filename = r'%s/%s.mp4' % (songer, song_name)
     # store_filename = store_filename_temp.replace(' ', '-')
     if os.path.exists(store_filename):
@@ -36,7 +41,7 @@ def main(num):
     else:
         os.mkdir('%s/' % songer)
         print 'Downloading The MV.....', store_filename
-        #os.mkdir(songer)
+        # os.mkdir(songer)
         urllib.urlretrieve(vedioUrls[0], store_filename, callbackfunc)
         print 'Downloading completed....'
 
