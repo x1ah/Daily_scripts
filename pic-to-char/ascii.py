f057634cce4b7cc5081f541d271ffdeb5ccaa735
Xia h <Xia h>
2016-04-15 16:34:05 +0800

    update

--- pic-to-char/ascii.py
+++ pic-to-char/ascii.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
-#coding=utf-8
+# coding=utf-8
 from PIL import Image
 import argparse
 import os
 
-#命令行输入参数处理
+# 命令行输入参数处理
 parser = argparse.ArgumentParser(description='trans picture to char')
 
-parser.add_argument('file')     #输入文件
-parser.add_argument('-o', '--output')   #输出文件
-parser.add_argument('--width', type = int, default = 80) #输出字符画宽
-parser.add_argument('--height', type = int, default = 30) #输出字符画高
+parser.add_argument('file')  # 输入文件
+parser.add_argument('-o', '--output')  # 输出文件
+parser.add_argument('--width', type=int, default=80)  # 输出字符画宽
+parser.add_argument('--height', type=int, default=30)  # 输出字符画高
 
-#获取参数
+# 获取参数
 args = parser.parse_args()
 
 IMG = args.file
@@ -20,10 +20,13 @@ WIDTH = args.width
 HEIGHT = args.height
 OUTPUT = args.output
 
-ascii_char = list("@#B%+&WM$×oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
+ascii_char = list(
+    "@#B%+&WM$×oahkbdpqwmZO0QLCJUYXzcvunxrjft/\|()1{}[]?-_+~<>i!lI;:,\"^`'. ")
 
 # 将256灰度映射到70个字符上
-def get_char(r,b,g,alpha = 256):
+
+
+def get_char(r, b, g, alpha=256):
     if alpha == 0:
         return ' '
     length = len(ascii_char)
@@ -35,20 +38,20 @@ def get_char(r,b,g,alpha = 256):
 if __name__ == '__main__':
 
     im = Image.open(IMG)
-    im = im.resize((WIDTH,HEIGHT), Image.NEAREST)
+    im = im.resize((WIDTH, HEIGHT), Image.NEAREST)
 
     txt = ""
 
     for i in range(HEIGHT):
         for j in range(WIDTH):
-            txt += get_char(*im.getpixel((j,i)))
+            txt += get_char(*im.getpixel((j, i)))
         txt += '\n'
 
     print txt
 
-    #字符画输出到文件
+    # 字符画输出到文件
     if OUTPUT:
-        with open(OUTPUT,'w') as f:
+        with open(OUTPUT, 'w') as f:
             f.write(txt)
     else:
         txtname = IMG.split('/')[-1].split('.')[0]+'.txt'
